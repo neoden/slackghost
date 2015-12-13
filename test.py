af74ebfdbae7ac63db67684b17a3bc9bdca50150
neoden <lenar.imamutdinov@gmail.com>
2015-12-13 16:54:21 +0300

    hello response

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 
 import requests
 import json
+import time
+import re
 from pprint import pprint
 
 import asyncio
@@ -12,32 +14,64 @@ API_URL = 'https://slack.com/api/'
 
 
 def method_url(name):
-	return API_URL + name
+    return API_URL + name
+
+
+class Bot():
+    def __init__(self, data):
+        self.url = data['url']
+        self.id = data['self']['id']
+        self.name = data['self']['name']
+        print('I am {}. My ID is {}'.format(self.name, self.id))
+
+    def handle(self, event):
+        pprint(event)
+        if event.get('type') == 'message':
+            return self.handle_message(event)
+
+    def handle_message(self, event):
+        if re.search('<@{}>'.format(self.id), event['text']):
+            print('I was mentioned in a message')
+            return json.dumps({
+                'type': 'message',
+                'channel': event['channel'],
+                'text': 'привет <@{}>'.format(event['user'])
+            })
 
 
 @asyncio.coroutine
-def listen(url):
-	websocket = yield from websockets.connect(url)
-	event = yield from websocket.recv()
-	pprint(event)
-	yield from websocket.close()
+def listen(bot):
+    websocket = yield from websockets.connect(bot.url)
+
+    while True:
+        event = yield from websocket.recv()
+        r = bot.handle(json.loads(event))
+        if r:
+            yield from websocket.send(r)
+
+        time.sleep(0.1)
+
+    yield from websocket.close()
 
 
 def main(token):
-	response = requests.get(method_url('rtm.start'), params={'token': token})
+    response = requests.get(method_url('rtm.start'), params={'token': token})
 
-	if response.status_code == 200:
-		r = json.loads(response.text)
-		ok = r['ok']
-		if ok:
-			websocket_url = r['url']
-			asyncio.get_event_loop().run_until_complete(listen(websocket_url))
-		else:
-			print('Connection failed')
-	else:
-		print(response.status_code)
+    if response.status_code == 200:
+        r = json.loads(response.text)
+        ok = r.get('ok')
+        if ok:
+            websocket_url = r['url']
+            bot = Bot(r)
+            asyncio.get_event_loop().run_until_complete(listen(bot))
+        else:
+            print('Connection failed with error: {}'.format(r.get('error')))
+    else:
+        print(response.status_code)
+
+
 
 
 if __name__ == '__main__':
-	token = open('token.txt').read().strip()
-	main(token)
+    token = open('token.txt').read().strip()
+    main(token)
